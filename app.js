5bfaf976fcd3b0f94b0c47209574fec6f3eba860
Anderson Nascimento <anderson.aje57@gmail.com>
2025-02-04 13:56:55 -0300

    Adicionando rota para aceitar dois parametros, nome e preço.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+app.get('/usuario/:id', (req, res) => {
+    const { id } = req.params; // Captura o ID da URL
+    res.send(`Usuário com ID: ${id}`);
+});
+ 
+app.get('/produto/:nome/:preco', (req, res) => {
+    const { nome, preco } = req.params;
+    res.send(`Produto: O ${nome} custa R$${preco}.`);
 });
 
 app.listen(PORT, () => {
